9006230b00a76911af46c40fdf5140201cda392f
yaswant7 <ykamineni@gmail.com>
2023-01-04 12:15:59 +0530

    Uploaded

---
+++ APITestApplication/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                Updatefun();
+                DelteDun();
+            }
+
+        }
+
+        private void DelteDun()
+        {
+            SqlConnection cn = null;
+            SqlCommand cmd = null;
+            try
+            {
+                cn = new SqlConnection();
+                cn.ConnectionString = @"data source=DESKTOP-YK\SQLEXPRESS; initial catalog =tsegrp_2_2022;integrated security=true";
+                int counter = 0;
+                cn.Open();
+
+
+                cmd = new SqlCommand();
+                cmd.Connection = cn;
+
+            }
+            catch(Exception ex)
+            {
+                Label1.Text = ex.Message;
+
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void Updatefun()
+        {
+            SqlConnection cn = null;
+            SqlCommand cmd = null;
+
+            cn = new SqlConnection();
+            cn.ConnectionString = @"data source=DESKTOP-YK\SQLEXPRESS; initial catalog =tsegrp_2_2022;integrated security=true";
+            int counter = 0;
+
+
+        }
+
+        public class secrets
+        {
+            public  String APP_KEY = "jk433g34hg3";
+
+            public String APP_SECRET = "987dwdqwdqw90";
+
+
+        } 
+    }
+}
